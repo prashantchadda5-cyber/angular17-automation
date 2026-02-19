84aa7c17268c21ff9770e2f727c14f2e3852ae90
prashant chadda <prashant.c@73strings.com>
2026-02-19 14:57:50 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  title = 123; // wrong type
-  username: string;
-  isLoggedIn: boolean = false;
+  title = 'Angular16 Demo';
+  username = '';
+  isLoggedIn = false;
 
   users = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Jane' }
   ];
 
-  users$: Observable<number[]>;
-
-  ngOnInit() {
-    this.users$ = of([1,2,3]).pipe(
-      map(value => {
-        return value.name; // invalid property
-      })
-    );
-
-    this.loadData();
-  }
-
-  loadData() {
-    setTimeout(function () {
-      this.isLoggedIn = "true"; // wrong context + type
-    }, 1000);
-  }
-
   submitForm() {
-    console.log(this.username.toFixed(2)); // string misuse
+    console.log(this.username);
+  }
+
+  trackById(index: number, user: any) {
+    return user.id;
   }
 }
