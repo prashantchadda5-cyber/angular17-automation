c3179c26f54cb50da4952fc09255542362af24bf
prashant chadda <prashant.c@73strings.com>
2026-02-19 15:16:58 +0530

    small hange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -14,6 +16,18 @@ export class AppComponent {
     { id: 1, name: 'John' },
     { id: 2, name: 'Jane' }
   ];
+
+  ngOnInit() {
+    const numbers$ = of(1, 2, 3, 4);
+
+   numbers$
+  .pipe(
+    map(value => value * 2)
+  )
+  .subscribe(result => {
+    console.log(result);
+  });
+  }
 
   submitForm() {
     console.log(this.username);
